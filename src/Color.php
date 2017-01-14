ad4e0ebf8c5817b2d6a4b207d952362ae3aa08d8
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-14 20:11:31 +0000

    validate hex string

--- src/Color.php
+++ src/Color.php
@@ -1,9 +1,29 @@
 <?php namespace jpuck\ColorMixer;
 
+use InvalidArgumentException;
+
 class Color
 {
     public function __construct($colorOrRed, $green = null, $blue = null)
     {
-        
+        $hex = $colorOrRed;
+        if ( ! static::validateHexColorString($hex) ) {
+            throw new InvalidArgumentException("$hex is not a valid string.");
+        }
+    }
+
+    public static function validateHexColorString(string $color) : bool
+    {
+        // optional leading hash #
+        $length = strlen( ltrim($color, '#') );
+
+        // must be 3 or 6 characters
+        switch ($length) {
+            case 3: break;
+            case 6: break;
+            default: return false;
+        }
+
+        return preg_match('/^#?[0-9a-fA-F]{3,6}$/', $color) === 1;
     }
 }
--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -15,10 +15,10 @@ class ColorTest extends PHPUnit_Framework_TestCase
             ['#ff0033'],
             ['FF0033'],
             ['#FF0033'],
-            [255, 0, 51],
-            ['100%','0%','20%'],
-            ['100%,0%,20%'],
-            ['100%, 0%, 20%'],
+//             [255, 0, 51],
+//             ['100%','0%','20%'],
+//             ['100%,0%,20%'],
+//             ['100%, 0%, 20%'],
         ];
     }
 
@@ -28,5 +28,41 @@ class ColorTest extends PHPUnit_Framework_TestCase
     public function testCanCreateColor($colorString)
     {
         $this->assertInstanceOf(Color::class, new Color($colorString));
+    }
+
+    public function invalidColorStringDataProvider()
+    {
+        return [
+            ['f033'],
+            ['#f033'],
+            ['F0333'],
+            ['#F0333'],
+            ['ff003s'],
+            ['#ff003s'],
+            ['FF003Z'],
+            ['#FF003Z'],
+        ];
+    }
+
+    /**
+     *  @dataProvider invalidColorStringDataProvider
+     */
+    public function testCanInvalidateColorString($colorString)
+    {
+        $this->assertFalse(Color::validateHexColorString($colorString));
+    }
+
+    public function invalidColorConstructorDataProvider()
+    {
+        return $this->invalidColorStringDataProvider();
+    }
+
+    /**
+     *  @dataProvider invalidColorConstructorDataProvider
+     */
+    public function testCanInvalidateColorConstructor($colorString)
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Color($colorString);
     }
 }
